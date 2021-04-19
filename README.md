b538b505782cc9e47278c84824e8e53f2088630f
Gricel Sepulveda <gricel.sepulveda@minsal.cl>
2021-04-19 13:58:20 -0400

    Logica encuesta

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # encuestas
 proyecto para hacer encuestas
+
+- En Windows instalar cmake para evitar errores y warnings: `https://cmake.org/install/`
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,61 @@
 //IMPORTS
-import React from "react"
+import React, { useState } from "react"
+//Components
+import Button from "../components/Button/Button"
+import Question from "./Question"
+//Styles
+import "../styles/home.scss"
+
+type Answer = {
+    id: number,
+    answer: string | undefined,
+    validate: boolean
+}
 
 const Home = () => {
+    const maxPage:number = 9
+    const [page, setPage] = useState<number | null>(1)
+    const [data, setData] = useState<Answer[] | null>([
+        {
+            id:1,
+            answer:undefined,
+            validate: false
+        }
+    ])
+
+    const handlePage = () => {
+        const nextPage:number = page != null ? page + 1 : 1
+        if (nextPage <= maxPage ){
+            setPage(nextPage)
+        }
+    }
+
+    const questions:string[] = [
+        'Tu edad',
+        'Tienes $10mil dólares a tu disposición. Los gastas:',
+        '¿A qué te dedicas?',
+        'Lo que más me quita el sueño es:',
+        '¿Cual es tu pelìcula/serie favorita y tu artista favorito?',
+        'Cuando bebes:',
+        'Uso frecuentemente',
+        'Para mi cumpleaños sería increíble si me regalaran:',
+        'Gracias por tu tiempo. Avisaremos el ganador por rrss y a tu correo'
+    ]
+
     return (
-        <>
-        </>
+        <div className="hw-form-pool">
+            {
+                questions.map((item, index) => 
+                    <Question
+                        activePage={page != null ? page : index + 1} page={index + 1} id={index + 1} validate={false}
+                        question={item} 
+                        body={<span>Componente</span>}
+                    />
+                )
+            }
+
+            <Button icon="" disabled={false} name="edad" value={`${page == 9 ? 'finalizar' : 'siguiente'}`} color="color-1" action={handlePage}/>
+        </div>
     )
 }
 
